cf174f0bfe38d63644e7cd8dcd51d54b7757bb40
lululepu <103906131+lululepu@users.noreply.github.com>
2023-06-27 01:14:08 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='selfpy',
+    name='diself',
     version='0.1',
     license='MIT',
     description='A python module like discord.py but for selfbot',
     long_description=open('README.md').read(),
     install_requires=['websocket-client', 'requests'],
-    url='https://github.com/lululepu/selfpy',
+    url='https://github.com/lululepu/diself',
     author='Lululepu',
     author_email='a.no.qsdf@gmail.com'
 )
